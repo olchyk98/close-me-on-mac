df487c2647dde158285118f9a26200ea131e05a2
Oles Odynets <olchyk98@gmail.com>
2024-09-14 20:51:23 +0200

    fix: kill apps gracefully
    This commit makes "kill" os command terminate
    processes with SIG 15 (graceful shutdown).
    way is more optimal, because some applications
    display "report crash" page when being killed
    with SIGTERM (default).

--- src/os/close-applications.ts
+++ src/os/close-applications.ts
@@ -3,8 +3,8 @@ import { exec } from './exec'
 
 export const __fn = async (pids: number[]): Promise<void> => {
   const payload = pids.join(' ')
-  const { error } = await exec(`kill ${payload}`)
-  if (error) throw Error(`Could not kill processes "${payload}": ${error}`)
+  const { error } = await exec(`kill -15 ${payload}`)
+  if (error) throw Error(`Could not close processes "${payload}": ${error}`)
 }
 
 export const closeApplications = safe(__fn)
